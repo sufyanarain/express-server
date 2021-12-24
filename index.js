059356539df821296d24a7da42aa8a08072a3cef
sufyanarain <sufyan822@gmail.com>
2021-12-24 18:40:33 +0500

    d'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const formidable = require("express-formidable");
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(formidable());
 
@@ -34,6 +34,10 @@ app.post("/submit", (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
